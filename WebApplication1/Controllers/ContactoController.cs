21a335faf7723a422106fc60660a9640939570f9
germannunez <german.nunez@ahinko.com>
2021-01-07 22:32:39 -0600

    feature: Prueba LAFISE

---
+++ WebApplication1/Controllers/ContactoController.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using WebApplication1.Models;
+
+namespace WebApplication1.Controllers
+{
+    public class ContactoController : Controller
+    {
+        private PRUEBAEntities db = new PRUEBAEntities();
+
+        // GET: Contactoedds
+        public ActionResult Index()
+        {
+            return View(db.Contacto.ToList());
+        }
+
+        // GET: Contactoedds/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Contacto contacto = db.Contacto.Find(id);
+            if (contacto == null)
+            {
+                return HttpNotFound();
+            }
+            return View(contacto);
+        }
+
+        // GET: Contactoedds/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Contactoedds/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "id,nombre,dni,apellido")] Contacto contacto)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Contacto.Add(contacto);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(contacto);
+        }
+
+        // GET: Contactoedds/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Contacto contacto = db.Contacto.Find(id);
+            if (contacto == null)
+            {
+                return HttpNotFound();
+            }
+            return View(contacto);
+        }
+
+        // POST: Contactoedds/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "id,nombre,dni,apellido")] Contacto contacto)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(contacto).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(contacto);
+        }
+
+        // GET: Contactoedds/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Contacto contacto = db.Contacto.Find(id);
+            if (contacto == null)
+            {
+                return HttpNotFound();
+            }
+            return View(contacto);
+        }
+
+        // POST: Contactoedds/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Contacto contacto = db.Contacto.Find(id);
+            db.Contacto.Remove(contacto);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
